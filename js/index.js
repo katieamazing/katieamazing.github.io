e43c56f1cc985865c96a14827c6ed0f9fa676029
katieamazing <k.allen@live.com>
2017-01-09 17:43:18 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+
+// init Isotope
+var grid = document.querySelector('.grid');
+
+var msnry = new Masonry( grid, {
+  itemSelector: '.grid-item',
+  columnWidth: 200,
+  fitWidth: true
+});
+
+imagesLoaded( grid ).on( 'progress', function() {
+  // layout Masonry after each image loads
+  msnry.layout();
+});
